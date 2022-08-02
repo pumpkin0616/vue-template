c62b666f75537523b9f4f41fa3c81f3749d29dcb
pumpkin0616 <61608531+pumpkin0616@users.noreply.github.com>
2022-08-02 16:54:48 +0800

    feat: vue3

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/pumpkin0616.github.io/'//这里写展示页面的仓库名称
-    : '/',
-}
+  ? '/pumpkin0616.github.io/'//这里写展示页面的仓库名称
+  : '/',
+})
